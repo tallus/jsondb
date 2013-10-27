63e1bf5bcb3c7ef2ed55d23e6914a6174f015f41
paulm <paulm@freegeek.org>
2013-10-26 17:41:49 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MiniDB",
+    version = "0.1",
+    find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+    packages = find_packages(),
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Paul Munday",
+    author_email = "contactme@paulmunday.net",
+    description = "Python module that loads a dictionary from a json file and allows you to access it as a simple nosql db like key value store. Heavily influenced by PickleDB.",
+    license = "GPL",
+    keywords = "json db key value store",
+    # could also include long_description, download_url, classifiers, etc.
+)
