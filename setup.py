21144238997c67c473588334da13de6eda7a3a3c
paulm <paulm@freegeek.org>
2013-11-01 16:57:12 -0700

    name change as MiniDB already exists

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = "MiniDB",
+    name = "JsonDB",
     version = "0.1",
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", 'tests.py']),
     package_data = {
