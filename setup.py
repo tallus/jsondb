669166b53b6dbf4f1638a4cfc4b040f5dee2ac7d
paulm <paulm@freegeek.org>
2013-11-02 14:54:50 -0700

    cleaned up

--- setup.py
+++ setup.py
@@ -4,12 +4,8 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "jsondb",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", 'tests.py', "*tests*"] ),
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-    },
 
     # metadata for upload to PyPI
     author = "Paul Munday",
